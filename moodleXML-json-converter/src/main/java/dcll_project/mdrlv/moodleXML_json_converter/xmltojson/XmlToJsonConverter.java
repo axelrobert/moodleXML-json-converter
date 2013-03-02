84f86db281ed02d07dc8a78443571b9cb64d8d25
Villard David <david0271@ymail.com>
2013-03-02 16:47:54 +0100

    Implémentation de la méthode de conversion et de son auxiliaire. Reste à la tester avec JUNIT en lui faisant passer un nom de fichier en entrée, en sortie et un nom de fichier XSLT XML ---> JSON à l'instanciation de la classe

--- moodleXML-json-converter/src/main/java/dcll_project/mdrlv/moodleXML_json_converter/xmltojson/XmlToJsonConverter.java
+++ moodleXML-json-converter/src/main/java/dcll_project/mdrlv/moodleXML_json_converter/xmltojson/XmlToJsonConverter.java
@@ -1,13 +1,21 @@
 package dcll_project.mdrlv.moodleXML_json_converter.xmltojson;
 
 import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URISyntaxException;
 
+import javax.xml.crypto.dsig.spec.XSLTTransformParameterSpec;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
 import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.transform.stream.StreamSource;
 
 import org.w3c.dom.Document;
 import org.xml.sax.SAXException;
@@ -15,7 +23,12 @@ import org.xml.sax.SAXException;
 import dcll_project.mdrlv.moodleXML_json_converter.WebStandardConverter;
 
 public class XmlToJsonConverter extends WebStandardConverter {
-
+	private String transformationStylesheet;
+	
+	public XmlToJsonConverter(String ch){
+		super();
+		transformationStylesheet=ch;
+	}
 	@Override
 	public boolean accordanceWithMoodleStandard(File f) {
 		// TODO Auto-generated method stub
@@ -51,7 +64,24 @@ public class XmlToJsonConverter extends WebStandardConverter {
 	public int convert(String inputFileUri, String outputFileUri)
 			throws IOException, URISyntaxException, TransformerException {
 		// TODO Auto-generated method stub
+		StreamSource inputFile=new StreamSource(new File(inputFileUri));
+		StreamSource xslTransformer=new StreamSource(new File(transformationStylesheet));
+		FileWriter outputFile=new FileWriter(new File(outputFileUri));
+		StreamResult out=new StreamResult(outputFile);
+		transformXmlToJsonViaXSLT(inputFile, xslTransformer, out);
 		return 0;
+	}
+	private void transformXmlToJsonViaXSLT(StreamSource inputFile,
+			StreamSource transformationStylesheet, StreamResult out) throws TransformerConfigurationException {
+		// TODO Auto-generated method stub
+		TransformerFactory transfact=TransformerFactory.newInstance();
+		Transformer transformer=transfact.newTransformer(transformationStylesheet);
+		try {
+			transformer.transform(inputFile, out);
+		} catch (TransformerException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 	
