1d51ba2bfa24810e8511c85a3b0380c99e689fc9
Villard David <david0271@ymail.com>
2013-03-02 16:22:09 +0100

    Création des 2 classes filles sans implémentation des méthodes abstraites

---
+++ moodleXML-json-converter/src/main/java/dcll_project/mdrlv/moodleXML_json_converter/jsontoxml/JsonToXmlConverter.java
@@ -0,0 +1,33 @@
+package dcll_project.mdrlv.moodleXML_json_converter.jsontoxml;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+import javax.xml.transform.TransformerException;
+
+import dcll_project.mdrlv.moodleXML_json_converter.WebStandardConverter;
+
+public class JsonToXmlConverter extends WebStandardConverter {
+
+	@Override
+	public boolean accordanceWithMoodleStandard(File f) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean accordanceWithStandard(File f) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int convert(String inputFileUri, String outputFileUri)
+			throws IOException, URISyntaxException, TransformerException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+
+}
