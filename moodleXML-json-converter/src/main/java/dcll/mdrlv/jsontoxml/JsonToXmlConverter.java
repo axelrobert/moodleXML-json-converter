2fc7980fe0a8e474ddf0139f413338d7752f6460
Villard David <david0271@ymail.com>
2013-03-10 03:21:26 +0100

    Implémentation de 2  méthodes héritées de la classe mère. Factorisation des blocs retournant un string xml compactée à partir d'un string json sous une fonction car utilisé à 2 reprises (moment du json test +  moment de la conversion)

--- moodleXML-json-converter/src/main/java/dcll/mdrlv/jsontoxml/JsonToXmlConverter.java
+++ moodleXML-json-converter/src/main/java/dcll/mdrlv/jsontoxml/JsonToXmlConverter.java
@@ -2,52 +2,115 @@ package dcll.mdrlv.jsontoxml;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.StringReader;
-import java.util.Scanner;
-
+import java.net.URISyntaxException;
 import javax.xml.transform.Result;
 import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.TransformerFactoryConfigurationError;
 import javax.xml.transform.sax.SAXSource;
 import javax.xml.transform.stream.StreamResult;
 
 import org.jdom2.Document;
+import org.jdom2.JDOMException;
 import org.jdom2.input.SAXBuilder;
 import org.jdom2.output.Format;
 import org.jdom2.output.XMLOutputter;
 import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
 
+import dcll.mdrlv.WebStandardConverter;
+import dcll.mdrlv.jsontoxml.JSONSaxAdapter.ParserException;
 import dcll.mdrlv.tools.Tools;
 
 
 
-public class JsonToXmlConverter {
+public class JsonToXmlConverter extends WebStandardConverter{
 
-	public static String convertToXml(final String json,
-			final String namespace, final boolean addTypeAttributes)
-			throws Exception {
+	public JsonToXmlConverter(){
+		super();
+	}
+	
+	public String convertJsonStringToCompactedXmlString(String json) {
 		ByteArrayOutputStream out = new ByteArrayOutputStream();
-		Transformer transformer = TransformerFactory.newInstance()
-				.newTransformer();
+		Transformer transformer = null;
+		try {
+			transformer = TransformerFactory.newInstance()
+					.newTransformer();
+		} catch (TransformerConfigurationException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (TransformerFactoryConfigurationError e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
 		InputSource source = new InputSource(new StringReader(json));
 		Result result = new StreamResult(out);
-		JSONXmlReader reader = new JSONXmlReader(namespace, addTypeAttributes);
+		JSONXmlReader reader = new JSONXmlReader("", false);
 		SAXSource sax = new SAXSource(reader, source);
-		transformer.transform(sax, result);
+		try {
+			try {
+				transformer.transform(sax, result);
+			} catch (TransformerException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		} catch (ParserException e) {
+			// TODO Auto-generated catch block
+			return "error";
+		}
 		return new String(out.toByteArray());
 	}
 
-	public static void main(String args[]) throws Exception {
-		Scanner scan = new Scanner(new File("ressources/exemple.json"));
-		String text = scan.useDelimiter("\\A").next();
-		scan.close();
-		String output = JsonToXmlConverter.convertToXml(text, "", false);
-		SAXBuilder sb=new SAXBuilder();
-		Tools.writeStringIntoFile(output, "results/output.xml");
-		Document doc=sb.build("results/output.xml");
-		XMLOutputter xmlOutputter = new XMLOutputter(Format.getPrettyFormat());
-		String xmlIndent = xmlOutputter.outputString(doc);
-		Tools.writeStringIntoFile(xmlIndent, "results/output.xml");
+	
+	@Override
+	public boolean accordanceWithMoodleStandard(File file)
+			throws FileNotFoundException, SAXException {
+		// TODO Auto-generated method stub
+		return false;
 	}
+
+	@Override
+	public boolean accordanceWithStandard(File file){
+		// TODO Auto-generated method stub
+		String json = Tools.readStringFromFile(file);
+		return ((!convertJsonStringToCompactedXmlString(json).contentEquals("error")));
+	}
+
+	@Override
+	public int convert(String inputFileUri, String outputFileUri)
+			throws IOException, URISyntaxException, TransformerException {
+		// TODO Auto-generated method stub
+		String text = Tools.readStringFromFile(new File(inputFileUri));
+		String output = convertJsonStringToCompactedXmlString(text);
+		Tools.writeStringIntoFile(output, "results/output.xml");
+		SAXBuilder sb = new SAXBuilder();
+		Document doc;
+		try {
+			doc = sb.build("results/output.xml");
+			XMLOutputter xmlOutputter = new XMLOutputter(Format.getPrettyFormat());
+			String xmlIndent = xmlOutputter.outputString(doc);
+			Tools.writeStringIntoFile(xmlIndent, "results/output.xml");
+		} catch (JDOMException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return 0;
+	}
+	
+	public static void main(String args[]) throws SAXException, IOException, URISyntaxException, TransformerException{
+		JsonToXmlConverter converter = new JsonToXmlConverter();
+		String testPath = "ressources/exemple.json";
+		File testFile = new File(testPath);
+		boolean validate = converter.accordanceWithStandard(testFile);
+		System.out.println(testPath + " is a " + validate + " json file." );
+		converter.convert(testPath, "results/output.xml");	
+	}
+	
 
 }
